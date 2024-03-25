40bddd2a578306ae743607935bff29c6da02f346
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-03-25 15:10:32 +0700

    finish

--- 077_FungsiSwitch/077_FungsiSwitch.cpp
+++ 077_FungsiSwitch/077_FungsiSwitch.cpp
@@ -48,29 +48,30 @@ int main()
 		cout << "5. Exit" << endl;
 		cout << "Masukan pilihan 1/2/3/4/5 = ";
 		cin >> pilihan;
-	} while (pilihan != 5);
 
-	switch (pilihan) {
-	case 1:
-		input();
-		cout << "hasil penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
-		break;
-	case 2:
-		input();
-		cout << "hasil pembagian = " << pembagian(bilSatu, bilDua) << endl;
-		break;
-	case 3:
-		input();
-		cout << "hasil perkalian = " << perkalian(bilSatu, bilDua) << endl;
-		break;
-	case 4:
-		input();
-		cout << "hasil pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
-		break;
-	case 5:
-		break;
-	default:
-		cout << "pilihan tidak ada" << endl;
-		break;
-	}
+
+		switch (pilihan) {
+		case 1:
+			input();
+			cout << "Hasil penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
+			break;
+		case 2:
+			input();
+			cout << "Hasil pembagian = " << pembagian(bilSatu, bilDua) << endl;
+			break;
+		case 3:
+			input();
+			cout << "Hasil perkalian = " << perkalian(bilSatu, bilDua) << endl;
+			break;
+		case 4:
+			input();
+			cout << "Hasil pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
+			break;
+		case 5:
+			break;
+		default:
+			cout << "pilihan tidak ada" << endl;
+			break;
+		}
+	} while (pilihan != 5);
 }
