4b31deef73ca9fc851bccf4af92b5f74b91099d1
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-03-25 14:29:39 +0700

    Add project files.

---
+++ 077_FungsiSwitch/077_FungsiSwitch.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+void input()
+{
+	cout << "Masukan Bilangan Pertama = ";
+	cin >> bilSatu;
+	cout << "Masukan Bilangan Kedua = ";
+	cin >> bilDua;
+}
+
